9ab02eea28dc1aabb3d2b45606c22882d1d7f242
Alexander Linné <21100826+lajne@users.noreply.github.com>
2017-11-05 17:46:20 +0100

    fill klar

--- paint/studentfill2.cpp
+++ paint/studentfill2.cpp
@@ -51,39 +51,45 @@ void exempelkodDemonstrerarDeque(){
 
 void nonRecursiveFillStack(int r, int k, IDrawingParent *im, QColor colorOld, QColor colorNew){
 
-    //exempelkodDemonstrerarDeque();
-
     deque<RK> stack;
     stack.push_back(RK(r, k));
+    if(colorNew == im->pixel(r, k)){
+        return;
+    }
     while (!stack.empty()) {
-        RK rk = stack.back();
-        stack.pop_back();
-        if(im->isInside(r, k) && colorOld == colorNew){
-            qDebug() << rk._r;
-            qDebug() << rk._k;
-            colorOld = colorNew;
-            stack.push_back(RK(r+1, k));
-            stack.push_back(RK(r-1, k));
-            stack.push_back(RK(r, k+1));
-            stack.push_back(RK(r, k-1));
-            im->setPixel(r+1, k, colorOld);
-            im->setPixel(r-1, k, colorOld);
-            im->setPixel(r, k+1, colorOld);
-            im->setPixel(r, k-1, colorOld);
+        RK rkCoord = stack.back();
+        if(im->isInside(rkCoord._r, rkCoord._k) && im->pixel(rkCoord._r,rkCoord._k) == colorOld){
+            im->setPixel(rkCoord._r, rkCoord._k, colorNew);
+            stack.push_back(RK(rkCoord._r+1, rkCoord._k));
+            stack.push_back(RK(rkCoord._r-1, rkCoord._k));
+            stack.push_back(RK(rkCoord._r, rkCoord._k+1));
+            stack.push_back(RK(rkCoord._r, rkCoord._k-1));
+        }
+        else {
             stack.pop_back();
         }
     }
-
-
-    /*
-     * im .pixel
-     * im .isInside
-     * im .setPixel
-     * */
 }
 
 
 void nonRecursiveFillFifo(int r, int k, IDrawingParent *im, QColor colorOld, QColor colorNew){
 
-    exempelkodDemonstrerarDeque();
+    deque<RK> queue;
+    queue.push_back(RK(r, k));
+    if(colorNew == im->pixel(r, k)){
+        return;
+    }
+    while (!queue.empty()) {
+        RK rkCoord = queue.front();
+        if(im->isInside(rkCoord._r, rkCoord._k) && im->pixel(rkCoord._r,rkCoord._k) == colorOld){
+            im->setPixel(rkCoord._r, rkCoord._k, colorNew);
+            queue.push_back(RK(rkCoord._r+1, rkCoord._k));
+            queue.push_back(RK(rkCoord._r-1, rkCoord._k));
+            queue.push_back(RK(rkCoord._r, rkCoord._k+1));
+            queue.push_back(RK(rkCoord._r, rkCoord._k-1));
+        }
+        else {
+            queue.pop_front();
+        }
+    }
 }
