a5e364408ee4dd6a6be8e6433fbd3c1c31591e30
Alexander Linné <21100826+lajne@users.noreply.github.com>
2017-11-08 15:35:55 +0100

    5.2 klar

--- treeProgram/student2.cpp
+++ treeProgram/student2.cpp
@@ -1,5 +1,3 @@
-
-
 #include "node.h"
 #include <stdio.h>
 #include <algorithm>
@@ -17,20 +15,57 @@
  **************************************************************************/
 
 void insertKeyToSearchtree(Node*& pTree, int key){
-
+    pTree->flash();
+    if(pTree == nullptr)
+        pTree = new Node(key, nullptr, nullptr);
+    else if (key < pTree->_key) {
+        insertKeyToSearchtree((pTree->_pLeft), key);
+    }
+    else if (key > pTree->_key) {
+        insertKeyToSearchtree((pTree->_pRight), key);
+    }
 }
 
 bool contains(Node *pTree, int key){
+    if (pTree==nullptr)
+        return false;
+    if (key < pTree->_key){
+        return contains(pTree->_pLeft, key);
+    }
+    if (key > pTree->_key){
+        return contains(pTree->_pRight, key);
+    }
     return true;
 }
 
 
 Node* nodeWithMinimumKey(Node *pTree){
-    return nullptr;
+    if (pTree == nullptr)
+        return nullptr;
+    if (pTree->_pLeft == nullptr)
+        return pTree;
+    return nodeWithMinimumKey(pTree->_pLeft);
 }
 
 void removeKeyFromSearchtree(Node* &pTree, int key){
-
+    pTree->flash();
+    if(pTree == nullptr)
+        return;
+    else if (key < pTree->_key) {
+        removeKeyFromSearchtree((pTree->_pLeft), key);
+    }
+    else if (key > pTree->_key) {
+        removeKeyFromSearchtree((pTree->_pRight), key);
+    }
+    else if(pTree->_pLeft == nullptr || pTree->_pRight == nullptr){
+        Node *pOldNode = pTree;
+        pTree = (pTree->_pLeft != nullptr? pTree->_pLeft : pTree->_pRight);
+        delete pOldNode;
+    }
+    else{
+        pTree->_key = nodeWithMinimumKey(pTree->_pRight)->_key;
+        removeKeyFromSearchtree((pTree->_pRight), pTree->_key);
+    }
 }
 
 
