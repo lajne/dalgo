8333e92cd7f884748a1a72b56f736a6dd02e8d58
Alexander Linné <21100826+lajne@users.noreply.github.com>
2017-11-12 03:10:17 +0100

    6.1 progress

--- treeProgram/student4.cpp
+++ treeProgram/student4.cpp
@@ -12,8 +12,41 @@
  **************************************************************************/
 
 bool didIndicatePathToTarget(Node *pTree){
-    return false;
+    int targetKey = 0;
+    if(pTree == nullptr)
+        return false;
+    pTree->flash();
+    didIndicatePathToTarget(pTree->_pLeft);
+    didIndicatePathToTarget(pTree->_pRight);
+    if(pTree->isTarget()){
+        pTree->makeRed();
+        targetKey = pTree->_key;
+    }
+    if(targetKey < pTree->_key){
+        pTree->makeRed();
+        didIndicatePathToTarget(pTree->_pLeft);
+    }
+    if(targetKey > pTree->_key){
+        pTree->makeRed();
+        didIndicatePathToTarget(pTree->_pRight);
+    }
+    return true;
 }
+//Hur fan ska jag få enbart vägen till noden att bli röd?
+
+//Den noden jag högerklickar på är väl den noden som jag får in i funktionen?
+
+//Hur ska jag veta vilka som skall vara röda?
+
+//Hade jag haft en key så hade denna varit så jävla enkel....
+
+
+
+
+
+
+
+
 
 
 
