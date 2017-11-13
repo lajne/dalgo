5e3f7467df165baa8c3cfa92b182362e4f8e8c5f
Alexander Linné <21100826+lajne@users.noreply.github.com>
2017-11-13 18:30:04 +0100

    6.1 done

--- treeProgram/student4.cpp
+++ treeProgram/student4.cpp
@@ -1,4 +1,3 @@
-
 #include "node.h"
 #include <stdio.h>
 #include <algorithm>
@@ -12,35 +11,24 @@
  **************************************************************************/
 
 bool didIndicatePathToTarget(Node *pTree){
-    int targetKey = 0;
+    pTree->flash();
     if(pTree == nullptr)
         return false;
-    pTree->flash();
-    didIndicatePathToTarget(pTree->_pLeft);
-    didIndicatePathToTarget(pTree->_pRight);
     if(pTree->isTarget()){
         pTree->makeRed();
-        targetKey = pTree->_key;
+        return true;
     }
-    if(targetKey < pTree->_key){
+    if(didIndicatePathToTarget(pTree->_pLeft)){
         pTree->makeRed();
-        didIndicatePathToTarget(pTree->_pLeft);
+        return true;
     }
-    if(targetKey > pTree->_key){
+    if(didIndicatePathToTarget(pTree->_pRight)){
         pTree->makeRed();
-        didIndicatePathToTarget(pTree->_pRight);
+        return true;
     }
-    return true;
+    didIndicatePathToTarget(pTree->_pLeft);
+    didIndicatePathToTarget(pTree->_pRight);
 }
-//Hur fan ska jag få enbart vägen till noden att bli röd?
-
-//Den noden jag högerklickar på är väl den noden som jag får in i funktionen?
-
-//Hur ska jag veta vilka som skall vara röda?
-
-//Hade jag haft en key så hade denna varit så jävla enkel....
-
-
 
 
 
